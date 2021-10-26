ba2c2720b06065a662a844911ffa771c47a3b935
sammy.uyghur <miever1@163.com>
2021-10-26 22:06:22 +0800

    input develop

---
+++ src/Components/Input/Input.stories.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Meta, Story } from '@storybook/react';
+
+import Input from './index';
+import { NativeInputProps } from './interface';
+
+export default {
+    title: 'Data Entry/Input',
+    component: Input
+} as Meta;
+
+const Template: Story<NativeInputProps> = (args) => <Input {...args} />;
+export const Basic = Template.bind({});
+Basic.args = {};
+
+export const StyleType: React.VFC<{}> = () => {
+    return (
+      <React.Fragment>
+        <Input size="sm" onChange={() => console.log(1)} disabled value={123} />
+        <Input size="lg" />
+      </React.Fragment>
+    )
+};
