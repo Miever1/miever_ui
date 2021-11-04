37af511cfe9f111e22756db99336752f95e525e1
sammy.uyghur <miever1@163.com>
2021-11-04 13:38:17 +0800

    Icon fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
 import Button from "./Components/Button";
 import Icon from "./Components/Icon";
 import Input from "./Components/Input";
 import Menu from "./Components/Menu";
 import Transition from "./Components/Transition";
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-library.add(fas);
 
 export {
   Button,
