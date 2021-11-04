8d1b67a98936d13452b590110367481f597af67e
sammy.uyghur <miever1@163.com>
2021-11-04 13:22:04 +0800

    add icon

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import Input from "./Components/Input";
 import Menu from "./Components/Menu";
 import Transition from "./Components/Transition";
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+
 export {
   Button,
   Icon,
