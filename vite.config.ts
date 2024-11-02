81133d20c8027e41dadf41253cc3082693c1cf67
miever <miever1@163.com>
2024-11-02 17:08:08 +0100

    feat: upgrade storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-// Example of dynamically setting the entry based on an environment variable
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const component = process.env.COMPONENT;  // Set this environment variable in your script
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: `src/${component}.tsx`,
-      name: component,
-      fileName: (format) => `${component}.${format}.js`
+      entry: 'src/index.tsx',  // This should be the entry point of your library
+      name: 'MieverUI',  // This is the global variable for UMD/IIFE format
+      fileName: (format) => `miever-ui.${format}.js`,
+      formats: ['es', 'umd'],  // Including UMD format
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
+        // Define globals for UMD build where external imports are needed
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM'
